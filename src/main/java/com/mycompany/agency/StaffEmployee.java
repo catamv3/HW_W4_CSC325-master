12e7c82fef75d33dcd69777f72e0aaeb5e49ae1a
sallyvalley <salmanmajid1975@gmail.com>
2023-09-21 19:28:15 -0400

    Completed to-dos

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -1,17 +1,14 @@
 package com.mycompany.agency;
 
 
-// TODO 1: Make StaffEmployee a child of StaffMember (done)
+
 
 public class StaffEmployee extends StaffMember
 {
     protected String socialSecurityNumber;
     protected double payRate;
 
-    //-----------------------------------------------------------------
-    //  TODO 2: Sets up this staff employee with the specified
-    //  information. (done)
-    //-----------------------------------------------------------------
+
     public StaffEmployee(String eName, String eAddress, String ePhone,
                          String socSecNumber, double rate)
     {
@@ -21,19 +18,14 @@ public class StaffEmployee extends StaffMember
         this.payRate = rate;
     }
 
-    //-----------------------------------------------------------------
-    // TODO 3: complete the implementation of toString to return information
-    //          about a staff employee as a string. (done)
-    //-----------------------------------------------------------------
+
     public String toString()
     {
-        return ("Name: " + super.name + " Address:  " + super.address + " Phone: " + super.phone +
+        return (super.toString() +
                 " SSN: " + socialSecurityNumber + " Pay Rate:  " + payRate);
     }
 
-    //-----------------------------------------------------------------
-    // TODO 4:  Returns the pay rate for this staff employee. (done)
-    //-----------------------------------------------------------------
+
     public double pay()
     {
         return payRate;
